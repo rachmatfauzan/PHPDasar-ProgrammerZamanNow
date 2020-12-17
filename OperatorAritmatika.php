bac3cc7d9c5a1ea6754481c607e1da4fddc0878a
Rachmat Fauzan <rachmatfauzan07@gmail.com>
2020-12-17 14:36:11 +0700

    Operator Penugasan

---
+++ OperatorAritmatika.php
@@ -0,0 +1,16 @@
+<?php  
+
+$result = 10 + 10;
+var_dump($result);
+
+$resultNegative = -$result;
+var_dump($resultNegative);
+
+$resultPositive = +$result;
+var_dump($resultPositive);
+
+// 
+$hasilSisa = 20 % 3;
+var_dump($hasilSisa);
+
+?>
---
+++ OperatorPenugasan.php
@@ -0,0 +1,17 @@
+<?php  
+
+$total =  0;
+
+$jeruk = 3000;
+$ayam = 15000;
+$capcin = 5000;
+
+
+$total += $jeruk;
+$total -= $capcin;
+$total += $ayam;
+var_dump("Rp ".number_format($total, '2', ',', '.'));
+
+
+
+?>
